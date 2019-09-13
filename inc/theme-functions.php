9291994e3334f186f3479a40b1b008992866ecac
Rakib Hossain <rakibhoossain@gmail.com>
2019-09-13 16:37:03 +0600

    post edit link

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -154,6 +154,25 @@ if( ! function_exists( 'magazine_pro_post_meta' ) ) {
 					        }
 				        }
 				    }
+
+				    if ( is_single() ){
+				    	edit_post_link(
+				    		sprintf(
+				    			wp_kses(
+				    				/* translators: %s: Name of current post. Only visible to screen readers */
+				    				__( 'Edit <span class="screen-reader-text">%s</span>', 'magazine-pro' ),
+				    				array(
+				    					'span' => array(
+				    						'class' => array(),
+				    					),
+				    				)
+				    			),
+				    			get_the_title()
+				    		),
+				    		'<li class="post-edit-link"><span class="meta-icon"><i class="feather icon-edit"></i></span>',
+				    		'</li>'
+				    	);
+				    }
 			        ?>
 		        </ul><!-- .post_meta -->
 		    </div><!-- .meta -->
